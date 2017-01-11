5c8e8cc3e1b3e2131832c01f341ac563b8fc08a6
许宝众 <403543653@qq.com>
2017-01-11 11:29:07 +0800

    首次提交

---
+++ demo-eureka-consumer/src/main/java/com/xbz/eureka/demo/consumer/ConsumerApplication.java
@@ -0,0 +1,17 @@
+package com.xbz.eureka.demo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableFeignClients
+public class ConsumerApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ demo-eureka-consumer/src/main/java/com/xbz/eureka/demo/consumer/HelloController.java
@@ -0,0 +1,16 @@
+package com.xbz.eureka.demo.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+	@Autowired
+	private HelloService helloServcie;
+	@RequestMapping("/test/{fallback}")
+	public String hello(@PathVariable("fallback") String fallback){
+		String res=helloServcie.hello(fallback);
+		return "调用服务结果为"+res;
+	}
+}
